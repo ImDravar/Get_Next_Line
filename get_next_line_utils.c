f8725722897caed539073cc0144f2ffd8f18244f
Ruben Ruiz Sanchez <rruiz-sa@cbr1s3.42barcelona.com>
2023-03-05 00:02:57 +0100

    save

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: rruiz-sa <rruiz-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 16:19:36 by rruiz-sa          #+#    #+#             */
-/*   Updated: 2023/03/04 18:08:42 by rruiz-sa         ###   ########.fr       */
+/*   Updated: 2023/03/04 21:37:07 by rruiz-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,33 +34,40 @@ size_t	ft_strlen(char *str)
 		i++;
 	return (i);
 }
-/*
-[Description ft_strlen]
-	1- Iterate the string, and return the counter(i)
-*/
 
 char	*ft_strjoin(char *s1, char *s2)
 {
 	int		i;
 	int		j;
 	char	*temp;
+	int		len;
 
-
-
-	if (ft_strlen(s1) == 0)
-		return ( (s2));
-	else if (ft_strlen(s2) == 0)
-		return (ft_strdup(s1));
+	i = -1;
+	j = -1;
+	if (!s1)
+	{
+		s1 = mallock(sizeof(char) + 1);
+		if (!s1)
+			return (0);
+		s1[0] = 0;
+	}
 	len = ft_strlen(s1) + ft_strlen(s2);
 	temp = (char *)malloc(sizeof(char) * len + 1);
 	if (!temp)
-		return (NULL);
-	ft_strlcpy(temp, s1, ft_strlen(s1)+1);
-	ft_strlcat(temp, s2, len + 1);
-	return (temp);
+		return (NULL); //Comprobar hacer un free
+	while (s1[++i])
+		temp[i] = s1[i];
+	while (s2[j++])
+		temp[i + j] = s2[j];
+	temp[i + j] = '\0';
+	//check limpiar s1
+	return (str);
 }
 
 /*
+[Description ft_strlen]
+	1- Iterate the string, and return the counter(i)
+
 [Description ft_strchr]
 	1- Iterate the pointer of the string
 	2- Every iteration check for matches with the character(c)
@@ -69,39 +76,16 @@ char	*ft_strjoin(char *s1, char *s2)
 		- If it matches return the pointer
 		- If not return (0)
 
-
-
-*/
-
-
-/*
-	1- If *s1 or *s2 equal to "" return the other.
-	2- Get the max size of the malloc with the sums of the two str lens.
-	3- Create the malloc (with +1 for the final \0) and check if it fails.
+[Description  ft_strjoin]
+	1- Check if *s1  exist.
+		{If doesn't}
+		a- Create a simple (and check) malloc and give a 0 value
+	3- Create the malloc with the size s1+s2(with +1 for the final \0)
+		and check if it fails.
+		{If fails}
+			a- Return NULL
 	4- Copy the *s1 inside the *temp.
 	5- Concat the *s2 inside the *temp.
-	[Description ft_strlen]
-		1- Iterate the string, and return the counter(i)
-	[Description ft_strdup]
-		1- Get the len of the source for the malloc
-		2- Create and check the new string
-		3- Copy in the new string the source
-		4- Return the new string
-	[Description ft_strlcpy]
-		1- Check if parameter lend_dst its 0 or negative
-			- If its return the len of str
-		2- Iterate while source exist and the counter its less to len_dst
-		3- Every iteration copy the source in the destination
-		4- When the iteration its over close the string
-		5- Return the len of the source
-	[Description ft_strlcat]
-		1- Get the len of the destination
-		2- If the size is < len of dst
-			- Return the len of source plus size
-		3- Iterate while
-			-(size > 0) and (destination len its smaller than size) and (source exist)
-		4- Every iteration copy the source in the destination (warning counters)
-		5- Close the string
-		6- Iterate while the rest of source exist and plus the destination len
-		7- Return destination len
-*/
+
+
+	*/
