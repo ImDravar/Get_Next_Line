8a2693c21dc7f02461fc09a86b6def7d59f11ba2
Ruben Ruiz Sanchez <rruiz-sa@car15s6.42barcelona.com>
2023-04-08 13:22:52 +0200

    Comments

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rruiz-sa <rruiz-sa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/22 17:56:27 by rruiz-sa          #+#    #+#             */
-/*   Updated: 2023/04/08 11:21:21 by rruiz-sa         ###   ########.fr       */
+/*   Updated: 2023/04/08 13:22:07 by rruiz-sa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,19 +37,6 @@ char	*ft_read(int fd, char *mini_box)
 	return (mini_box);
 }
 
-/*
-[Description ft_read]
-	1- Create a mallow
-		{If doesn't}
-			a- Return Null
-	2- Iterate if bytes != 0 and the buffer are not an '\n'
-		- Every run save in the variable the fd read count,
-		   and check if the bytes readed > 0
-			{If its}
-				-Close the buffer and add the content to the string box
-			{If doesn't}
-				-Clean the strs memory
-*/
 
 char	*ft_cut(char *mini_box)
 {
@@ -87,8 +74,6 @@ char	*get_next_line(int fd)
 	i = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!box)
-		box = ft_strdup("");
 	box = ft_read(fd, box);
 	if (!box)
 		return (NULL);
@@ -104,7 +89,49 @@ char	*get_next_line(int fd)
 	}
 	return (line);
 }
+
 /*
+[Description get_next_line]
+	1-Check if the fd or the BUFFER_SIZE are <0
+		{If its}
+			a-Return NULL
+	2-Read the fd and put inside of the string static
+	3-Check if the static exist
+	4-Iterate while container have content and its not a \n
+	5-With the firts \n detection, put the rest inside a temp string
+	6-Cut the line from the static and put inside again
+	7- Check if line[0] exist
+		{If doesn't}
+			a-Free line and static
+	8- Return line
+
+[Description ft_read]
+	1- Create a mallow and check if its exist
+		{If doesn't}
+			a- Return Null
+	2- Iterate if bytes != 0 and the buffer are not an '\n'
+		- Every run save in the variable the fd read count,
+		   and check if the bytes readed > 0
+			{If its}
+				-Close the buffer and add the content to the string box
+			{If doesn't}
+				-Clean the strs memory
+
+[Description ft_cut]
+	1-Iterate while str exist and its not a \n
+		-Save counter (this save where is the \n)
+	2-Check if Str[cont] stills exist
+		{If doesn't}
+			a- free str and return NULL
+	3-Check if Str[cont] its equalt to \n
+		{If its}
+			a- iterate cont
+	4- Create and check malloc size of len_str - cont(+1 for \0)
+	5-Iterate while str[cont] exist and put the content in a temp str
+	6- Out of the iteration close the temp
+	7- Free the str and return temp
+
+
 [Description get_next_line]
 	1- Create (and check)the malloc with the BUFFER_SIZE
 	BUFFER_SIZE
